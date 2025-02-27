012adccee1b1b9c6392d75dca488478d4476b4d5
waspryo <wasp2123321@gmail.com>
2025-02-27 21:25:51 +0900

    Add hooks library to monorepo
    - Create hooks project as a shared React hooks library
    - Configure TypeScript, Babel, and ESLint settings
    - Set up library build and project configuration
    - Update workspace tsconfig to include new hooks project

---
+++ libs/hooks/src/lib/hooks.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const StyledHooks = styled.div`
+  color: pink;
+`;
+export function Hooks() {
+  return (
+    <StyledHooks>
+      <h1>Welcome to Hooks!</h1>
+    </StyledHooks>
+  );
+}
+
+export default Hooks;
